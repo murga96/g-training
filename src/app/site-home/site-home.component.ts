74ca68746b845754b71c0b782793b80b11a33882
Gustavo Martínez de Murga <gustavo.murga1996@gmail.com>
2022-10-12 14:07:01 -0400

    Creating tooltip button with innercontent

--- src/app/site-home/site-home.component.ts
+++ src/app/site-home/site-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -8,13 +8,27 @@ import { Router } from '@angular/router';
   styleUrls: ['./site-home.component.scss'],
 })
 export class SiteHomeComponent implements OnInit {
+  @ViewChild('button', {static: false}) button !: ElementRef;
+  @ViewChild('tooltip', {static: false}) tooltip !: ElementRef;
   numberControl: FormControl = this.fb.control('', [
     Validators.required,
     Validators.min(1),
     Validators.max(100),
   ]);
-  constructor(private fb: FormBuilder, private router: Router) {}
+  tooltipAppear: Object = {}
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+  
+  onMouseLeave() {
+    console.log("button leave")
+    this.tooltip.nativeElement.innerHTML = ""
+    this.tooltipAppear = {visibility: 'hidden', opacity: '0'}
+  }
+  onMouseEnter() {
+    this.tooltipAppear = {visibility: 'visible', opacity: '1'}
+    this.tooltip.nativeElement.innerHTML = this.button?.nativeElement.innerHTML
+    console.log("button enter")
+  }
   onClick() {
     this.router.navigate(["site-restricted"], { queryParams: { number: this.numberControl.value } })
   }
