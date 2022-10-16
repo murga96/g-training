1dbcff32a80cb9118c831e67685ebee1bf451408
Gustavo Martínez de Murga <gustavo.murga1996@gmail.com>
2022-10-16 13:55:35 -0400

    Fixing width in container

--- src/app/site-restricted/site-restricted.component.ts
+++ src/app/site-restricted/site-restricted.component.ts
@@ -21,6 +21,7 @@ export class SiteRestrictedComponent {
   }
 
   ngAfterViewInit(): void {
+    console.log(this.square.nativeElement.offsetWidth, 'jjj')
     Promise.resolve().then(() => /* { */
      ( this.height = this.square.nativeElement.offsetWidth)
       // this.margin = this.square.nativeElement.offsetWidth / 2
